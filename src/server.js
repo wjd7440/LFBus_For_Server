c34d0d87c46f53e0d533dcc85e1ef5f40f8b351b
wjd7440 <50983731+wjd7440@users.noreply.github.com>
2020-07-22 17:19:47 +0900

    jjh_20200722 17:19

--- src/server.js
+++ src/server.js
@@ -1,22 +1,40 @@
-require("dotenv").config();
+import "./env";
+
 import { GraphQLServer } from "graphql-yoga";
+import logger from "morgan";
+import schema from "./schema";
+import "./passport";
+import { authenticateJwt } from "./passport";
+import { isUserAuthenticated, isAdminAuthenticated } from "./middlewares";
+import express from "express";
 
 const PORT = process.env.PORT || 4000;
 
-const typeDefs = `
-    type Query{
-        hello: String!
-    }
-`;
+const server = new GraphQLServer({
+  schema,
+  context: ({ request }) => ({
+    request,
+    isUserAuthenticated,
+    isAdminAuthenticated,
+  }),
+});
 
-const resolvers = {
-  Query: {
-    hello: () => "hi",
-  },
-};
-
-const server = new GraphQLServer({ typeDefs, resolvers });
+server.express.use(logger("dev"));
+server.express.use(
+  logger(function (tokens, req, res) {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, "content-length"),
+      "-",
+      tokens["response-time"](req, res),
+      "ms",
+    ].join(" ");
+  })
+);
+server.express.use(authenticateJwt);
 
 server.start({ port: PORT }, () =>
-  console.log(`Server running on port http://localhost:${PORT}`)
+  console.log(`✅ Server running on http://localhost:${PORT}`)
 );
