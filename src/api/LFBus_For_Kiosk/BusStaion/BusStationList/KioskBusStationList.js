551287e5c57c82d0d24728f91a2240368c5cb670
wjd7440 <50983731+wjd7440@users.noreply.github.com>
2020-07-22 17:58:41 +0900

    jjh_20200722 17:58

---
+++ src/api/LFBus_For_Kiosk/BusStaion/BusStationList/KioskBusStationList.js
@@ -0,0 +1,32 @@
+import { prisma } from "../../../../../generated/prisma-client";
+
+export default {
+  Query: {
+    KioskBusStationList: async (_, args) => {
+      const { keyword, orderBy, skip, after, before, first, last } = args;
+
+      //   if (keyword) {
+      //     where = { ...where, question_contains: keyword };
+      //   }
+
+      const busStations = await prisma.busStations({
+        where,
+        orderBy,
+        skip, //offset
+        after,
+        before,
+        first, //limit
+        last,
+      });
+
+      const count = await prisma
+        .busStationsConnection({
+          where,
+        })
+        .aggregate()
+        .count();
+
+      return { busStations, count };
+    },
+  },
+};
