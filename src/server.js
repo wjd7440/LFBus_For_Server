3fdf4bac2cd4abe23c2ce969046eab68a8b67ddb
wjd7440 <50983731+wjd7440@users.noreply.github.com>
2020-07-22 14:04:49 +0900

    jjh_20200722 14:04

--- src/server.js
+++ src/server.js
@@ -3,19 +3,31 @@ import { GraphQLServer } from "graphql-yoga";
 
 const PORT = process.env.PORT || 4000;
 
-const typeDefs = `
-    type Query{
-        hello: String!
-    }
-`;
+const server = new GraphQLServer({
+  schema,
+  context: ({ request }) => ({
+    request,
+    isKioskAuthenticated,
+    isRiderAuthenticated,
+    isUserAuthenticated,
+    isAdminAuthenticated,
+  }),
+});
 
-const resolvers = {
-  Query: {
-    hello: () => "hi",
-  },
-};
-
-const server = new GraphQLServer({ typeDefs, resolvers });
+server.express.use(logger("dev"));
+server.express.use(
+  logger(function (tokens, req, res) {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, "content-length"),
+      "-",
+      tokens["response-time"](req, res),
+      "ms",
+    ].join(" ");
+  })
+);
 
 server.start({ port: PORT }, () =>
   console.log(`Server running on port http://localhost:${PORT}`)
