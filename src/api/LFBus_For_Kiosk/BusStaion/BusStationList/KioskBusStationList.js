df75c76868f60a686b5b19ec6c2795c7d0a5bb94
wjd7440 <50983731+wjd7440@users.noreply.github.com>
2020-07-24 16:03:29 +0900

    jjh_20200724 16:03

--- src/api/LFBus_For_Kiosk/BusStaion/BusStationList/KioskBusStationList.js
+++ src/api/LFBus_For_Kiosk/BusStaion/BusStationList/KioskBusStationList.js
@@ -20,9 +20,7 @@ export default {
       });
 
       const count = await prisma
-        .busStationsConnection({
-          where,
-        })
+        .busStationsConnection()
         .aggregate()
         .count();
 
