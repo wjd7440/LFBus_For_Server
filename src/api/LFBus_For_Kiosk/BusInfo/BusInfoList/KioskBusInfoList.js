6b69fff09070059895e6a9c85498f9a194e89feb
jungstar <50983731+wjd7440@users.noreply.github.com>
2020-08-10 17:38:13 +0900

    jjh_20200810 17:38

--- src/api/LFBus_For_Kiosk/BusInfo/BusInfoList/KioskBusInfoList.js
+++ src/api/LFBus_For_Kiosk/BusInfo/BusInfoList/KioskBusInfoList.js
@@ -3,12 +3,12 @@ import { prisma } from "../../../../../generated/prisma-client";
 export default {
   Query: {
     KioskBusInfoList: async (_, args) => {
-      const { CAR_REG_NUM } = args;
+      const { CAR_REG_NO } = args;
 
-      const busInfoes = await prisma.busInfoes({where : CAR_REG_NUM});
+      const busInfoes = await prisma.busInfoes({where : CAR_REG_NO});
 
       const count = await prisma
-        .busInfoesConnection({where : CAR_REG_NUM})
+        .busInfoesConnection({where : CAR_REG_NO})
         .aggregate()
         .count();
 
