c7e12a56fa4fafd620d5b7f1566a1a327519badd
jungstar <50983731+wjd7440@users.noreply.github.com>
2020-08-11 11:38:05 +0900

    jjh_20200811 11:38

---
+++ src/api/LFBus_For_Kiosk/BusInfo/BusInfoList/KioskBusInfo.js
@@ -0,0 +1,10 @@
+import { prisma } from "../../../../../generated/prisma-client";
+
+export default {
+  Query: {
+    KioskBusInfo: async (_, args) => {      
+      const { CAR_REG_NO } = args;
+      return await prisma.busInfo({ CAR_REG_NO });
+    }
+  }
+};
