6fb2db6471c383466d283f78bb4f5434cb78c703
jungstar <50983731+wjd7440@users.noreply.github.com>
2020-08-14 12:04:48 +0900

    jjh_20200814 12:04

--- src/api/LFBus_For_User/BusStaion/BusStationList/UserBusStationList.js
+++ src/api/LFBus_For_User/BusStaion/BusStationList/UserBusStationList.js
@@ -2,7 +2,7 @@ import { prisma } from "../../../../../generated/prisma-client";
 
 export default {
   Query: {
-    KioskBusStationList: async (_, args) => {
+    UserBusStationList: async (_, args) => {
       // const { keyword, orderBy, skip, after, before, first, last } = args;
 
       // let where = null;
@@ -13,10 +13,7 @@ export default {
 
       const busStations = await prisma.busStations();
 
-      const count = await prisma
-        .busStationsConnection()
-        .aggregate()
-        .count();
+      const count = await prisma.busStationsConnection().aggregate().count();
 
       return { busStations, count };
     },
