318f22707fa256da2d290daaba08f380dc86bc5b
jungstar <50983731+wjd7440@users.noreply.github.com>
2020-08-21 15:50:21 +0900

    jjh_20200821 15:50

--- src/api/LFBus_For_User/BusStaion/BusStationList/UserBusStationList.js
+++ src/api/LFBus_For_User/BusStaion/BusStationList/UserBusStationList.js
@@ -11,28 +11,23 @@ export default {
       let busStations = [];
       const connection = await pool.getConnection(async (conn) => conn);
       const objects = await connection.query(
-        `SELECT BUS_NODE_ID, BUSSTOP_NM, distance_between(GPS_LATI, GPS_LONG, ${latitude}, ${longitude}) AS DISTANCE FROM BusStation WHERE distance_between(GPS_LATI, GPS_LONG, ${latitude}, ${longitude}) <= 0.5 `,
-        function(error, results, fields) {
-          if (error) throw error;
-          // console.log(results[0].BUS_NODE_ID);
-          results.map((obejct, index) => {
-            const busStation = {
-              BUS_NODE_ID: obejct.BUS_NODE_ID,
-              BUSSTOP_NM: obejct.BUSSTOP_NM,
-              DISTANCE: obejct.DISTANCE,
-            };
-            console.log(busStation);
-            // busStations.push({
-            //   BUS_NODE_ID: obejct.BUS_NODE_ID,
-            //   BUSSTOP_NM: obejct.BUSSTOP_NM,
-            //   DISTANCE: obejct.DISTANCE,
-            // });
-          });
-        }
+        `SELECT BUS_NODE_ID, BUSSTOP_NM, distance_between(GPS_LATI, GPS_LONG, ${latitude}, ${longitude}) AS DISTANCE FROM BusStation WHERE distance_between(GPS_LATI, GPS_LONG, ${latitude}, ${longitude}) <= 0.5 `
+        // (error, results, fields) => {
+        //   if (error) throw error;
+        //   // console.log(results[0].BUS_NODE_ID);
+        //   results.map((obejct, index) => {
+        //     const busStation = {
+        //       BUS_NODE_ID: obejct.BUS_NODE_ID,
+        //       BUSSTOP_NM: obejct.BUSSTOP_NM,
+        //       DISTANCE: obejct.DISTANCE,
+        //     };
+        //     busStations.push(busStation);
+        //   });
+        // }
       );
       connection.release();
 
-      console.log(busStations);
+      console.log(objects);
 
       const count = busStations.length;
 
