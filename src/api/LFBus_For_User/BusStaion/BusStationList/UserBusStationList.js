44dc3f427dde9700c99e3d298e9af0235d605cfe
jungstar <50983731+wjd7440@users.noreply.github.com>
2020-08-21 15:31:52 +0900

    jjh_20200821 15:31

--- src/api/LFBus_For_User/BusStaion/BusStationList/UserBusStationList.js
+++ src/api/LFBus_For_User/BusStaion/BusStationList/UserBusStationList.js
@@ -8,18 +8,20 @@ export default {
     UserBusStationList: async (_, args) => {
       const { latitude, longitude } = args;
 
+      let busStations = [];
       const connection = await pool.getConnection(async (conn) => conn);
       const objects = await connection.query(
         `SELECT BUS_NODE_ID, BUSSTOP_NM, distance_between(GPS_LATI, GPS_LONG, ${latitude}, ${longitude}) AS DISTANCE FROM BusStation WHERE distance_between(GPS_LATI, GPS_LONG, ${latitude}, ${longitude}) <= 0.5 `,
-        []
+        function(error, results, fields) {
+          if (error) throw error;
+
+          results.map((obejct, index) => {
+            busStations.push(obejct);
+          });
+        }
       );
       connection.release();
 
-      let busStations = [];
-      objects.map((obejct, index) => {
-        busStations.push(obejct.TextRow);
-      });
-      console.log(objects);
       console.log(busStations);
 
       const count = busStations.length;
