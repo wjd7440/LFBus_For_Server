0ffeb6f90b410cf988b9f89e7700b4e3f1945c01
jungstar <50983731+wjd7440@users.noreply.github.com>
2020-09-02 10:35:50 +0900

    jjh_20200902 10:35

---
+++ src/api/LFBus_For_User/Reservation/ReservationList/UserReservationList.js
@@ -0,0 +1,34 @@
+import { prisma } from "../../../../../generated/prisma-client";
+
+export default {
+  Query: {
+    UserReservationList: async (_, args, { request, isUserAuthenticated }) => {
+      isUserAuthenticated(request);
+      const { orderBy, skip, after, before, first, last } = args;
+      const { account } = request;
+
+      let where = {
+        userId: { id: account.id },
+      };
+
+      const reservations = await prisma.reservations({
+        where,
+        orderBy,
+        skip, //offset
+        after,
+        before,
+        first, //limit
+        last,
+      });
+
+      const count = await prisma
+        .reservationsConnection({
+          where,
+        })
+        .aggregate()
+        .count();
+
+      return { reservations, count };
+    },
+  },
+};
