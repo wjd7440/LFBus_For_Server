2a276d017784a82cdcf3ef01eeeb793db19330ec
jungstar <50983731+wjd7440@users.noreply.github.com>
2020-10-13 15:31:25 +0900

    jjh_20201013 15:31

--- src/api/LFBus_For_User/BusRoute/BusRouteList/UserBusRoute.js
+++ src/api/LFBus_For_User/BusRoute/BusRouteList/UserBusRoute.js
@@ -2,8 +2,25 @@ import { prisma } from "../../../../../generated/prisma-client";
 
 export default {
   Query: {
-    UserBusRoute: async (_) => {
-      return await prisma.busRoute();
+    UserBusRouteList: async (_) => {
+      const { orderBy, skip, after, before, first, last } = args;
+      
+      const busRoutes = await prisma.busRoutes({
+        where,
+        orderBy,
+        skip, //offset
+        after,
+        before,
+        first, //limit
+        last,
+      });
+
+      const count = await prisma
+        .busRoutesConnection()
+        .aggregate()
+        .count();
+
+      return { busRoutes, count };
     },
   },
 };
