c298c5eea395e19e6d0cc4e569e352f5190befc6
jungstar <50983731+wjd7440@users.noreply.github.com>
2020-10-13 15:37:31 +0900

    jjh_20201013 15:37

--- src/api/LFBus_For_User/BusRoute/BusRouteList/UserBusRoute.js
+++ src/api/LFBus_For_User/BusRoute/BusRouteList/UserBusRoute.js
@@ -6,7 +6,6 @@ export default {
       const { orderBy, skip, after, before, first, last } = args;
       
       const busRoutes = await prisma.busRoutes({
-        where,
         orderBy,
         skip, //offset
         after,
