b555f8dc3ba5059af33b6b8d5de837a690e1d4cb
jungstar <50983731+wjd7440@users.noreply.github.com>
2020-11-03 15:14:12 +0900

    jjh_20201103 15:14

--- src/api/LFBus_For_User/BusStaion/BusStationSearchList/UserBusStationSearchList.js
+++ src/api/LFBus_For_User/BusStaion/BusStationSearchList/UserBusStationSearchList.js
@@ -1,22 +1,34 @@
 import { prisma } from "../../../../../generated/prisma-client";
+import mysql from "mysql2/promise";
+import { dbConfig } from "../../../../../config/db.config";
+const pool = mysql.createPool(dbConfig);
 
 export default {
   Query: {
     UserBusStationSearchList: async (_, args) => {
-      // const { keyword, orderBy, skip, after, before, first, last } = args;
+      const { latitude, longitude, orderBy } = args;
 
-      // let where = null;
+      let busStations = [];
+      const connection = await pool.getConnection(async (conn) => conn);
+      const [results] = await connection.query(
+        `SELECT BUS_STOP_ID ,BUS_NODE_ID, BUSSTOP_NM, GPS_LATI, GPS_LONG, distance_between(GPS_LATI, GPS_LONG, ${latitude}, ${longitude}) AS DISTANCE FROM BusStation WHERE distance_between(GPS_LATI, GPS_LONG, ${latitude}, ${longitude}) <= 200 ORDER BY DISTANCE ASC `
+      );
+      connection.release();
 
-      //   if (keyword) {
-      //     where = { ...where, question_contains: keyword };
-      //   }
+      results.map((obejct, index) => {
+        const busStation = {
+          BUS_STOP_ID: obejct.BUS_STOP_ID,
+          BUS_NODE_ID: obejct.BUS_NODE_ID,
+          BUSSTOP_NM: obejct.BUSSTOP_NM,
+          DISTANCE: obejct.DISTANCE,
+          GPS_LATI: obejct.GPS_LATI,
+          GPS_LONG: obejct.GPS_LONG,
+          orderBy: orderBy,
+        };
+        busStations.push(busStation);
+      });
 
-      const busStations = await prisma.busStations();
-
-      const count = await prisma
-        .busStationsConnection()
-        .aggregate()
-        .count();
+      const count = busStations.length;
 
       return { busStations, count };
     },
