ffb97df760a26d7e437dbd0f684d1d6b66288f93
jungstar <50983731+wjd7440@users.noreply.github.com>
2020-11-18 17:57:22 +0900

    jjh_20201118 17:57

--- src/api/LFBus_For_Rider/Reservation/ReservationNotice/RiderReservationNotice.js
+++ src/api/LFBus_For_Rider/Reservation/ReservationNotice/RiderReservationNotice.js
@@ -21,19 +21,19 @@ export default {
       });
       console.log("예약")          
       console.log(reservations)          
-      const count = await prisma
-        .reservationsConnection({
-          where,
-        })
-        .aggregate()
-        .count();        
-        console.log("1 : " + count)
-        if(!count) {
-          await prisma.updateReservation({
-            data: { notice: true },
-            where: { id: reservations[0].id }
-          });
-        }  
+      // const count = await prisma
+      //   .reservationsConnection({
+      //     where,
+      //   })
+      //   .aggregate()
+      //   .count();        
+      //   console.log("1 : " + count)
+      //   if(!count) {
+      //     await prisma.updateReservation({
+      //       data: { notice: true },
+      //       where: { id: reservations[0].id }
+      //     });
+      //   }  
 console.log("2 : " + count)
       return { reservations, count };
     },
