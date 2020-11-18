03991c26f20efc5a7b2fc0ac91c9b394c9b73c9f
jungstar <50983731+wjd7440@users.noreply.github.com>
2020-11-18 10:57:56 +0900

    jjh_20201118 10:58

---
+++ src/api/LFBus_For_Rider/Reservation/ReservationNotice/RiderReservationNotice.js
@@ -0,0 +1,33 @@
+import { prisma } from "../../../../../generated/prisma-client";
+
+export default {
+  Query: {
+    RiderReservationList: async (_, args, { request, isUserAuthenticated }) => {
+      const { orderBy, skip, after, before, first, last, CAR_REG_NO } = args;
+
+      let where = {
+        CAR_REG_NO: CAR_REG_NO,
+        notice: true,
+      };
+
+      const reservations = await prisma.reservations({
+        where,
+        orderBy,
+        skip, //offset
+        after,
+        before,
+        first, //limit
+        last,
+      });
+
+      const count = await prisma
+        .reservationsConnection({
+          where,
+        })
+        .aggregate()
+        .count();
+
+      return { reservations, count };
+    },
+  },
+};
