4e353e25902bffb34639e8a4e01a536afb9ffa28
jungstar <50983731+wjd7440@users.noreply.github.com>
2020-11-30 14:55:44 +0900

    jjh_20201130 14:56

--- src/api/LFBus_For_Admin/Dashboard.js
+++ src/api/LFBus_For_Admin/Dashboard.js
@@ -1,13 +1,4 @@
 import { prisma } from "../../../generated/prisma-client";
 
 export default {
-  Event: {
-    eventConfig: ({ id }) => prisma.event({ id }).eventConfig()
-  },
-  Notice: {
-    noticeConfig: ({ id }) => prisma.notice({ id }).noticeConfig()
-  },
-  Inquiry: {
-    inquiryConfig: ({ id }) => prisma.inquiry({ id }).inquiryConfig()
-  }
 };
